0ec4fd264e3aefc17e58c36f89c895d61860306a
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-08-23 19:14:17 -0500

    Added acronym and organization_title functions

---
+++ acronym.js
@@ -0,0 +1,13 @@
+function() {
+	var word = this.title; // set word e.g. "American Biological Society"
+	var isUppercase = true; // set uppercase
+
+	var words = word.split(" ");
+	var acronym = "";
+
+	for (var i = 0; i < words.length; i++) {
+		acronym += words[i].charAt(0).toUpperCase();
+	}
+
+	return acronym;
+}
---
+++ organization_title.js
@@ -0,0 +1,23 @@
+function() {
+	var minWords = 1;
+	var maxWords = 6;
+	var words = lorem(maxWords, "words").split(" ");
+	var amount = minWords + Math.floor(Math.random() * maxWords);
+
+	// remove random amount of words
+	words = words.splice(0, amount || 0);
+
+	// concatenate word together
+	var title = "";
+	for (var i = 0; i < words.length; i++) {
+		title += words[i] + " "; 
+	}
+
+	// remove last space character
+	title = title.slice(0, -1);
+
+	// capitalize the first characters of each word
+	title = title.replace(/\b\w/g, function(l){ return l.toUpperCase();});
+
+	return title;
+}
